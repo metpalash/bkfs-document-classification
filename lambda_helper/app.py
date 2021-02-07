f1141d4d4bba9d5567ce5d73189297b9b373cc4e
Palash Jain <palash@Palashs-MBP.attlocal.net>
2021-02-06 21:35:16 -0500

    lambda handler

---
+++ lambda_helper/app.py
@@ -0,0 +1,73 @@
+print('container start')
+try:
+  import unzip_requirements
+except ImportError:
+  pass
+print('unzip end')
+
+import json
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.svm import LinearSVC
+from sklearn.metrics import accuracy_score, classification_report
+from joblib import dump, load
+import boto3
+import os
+import tempfile
+# import requests
+
+#Pull up the env variables
+MODEL_BUCKET_NAME = os.environ['MODEL_BUCKET_NAME']
+MODEL_FILE_NAME_KEY = os.environ['MODEL_FILE_NAME_KEY']
+TEMP_DIR = '/tmp' 
+MODEL_PATH = os.path.join(TEMP_DIR, MODEL_FILE_NAME_KEY)
+
+#Download model from S3
+print('downloading model...')
+s3 = boto3.resource('s3')
+s3.Bucket(MODEL_BUCKET_NAME).download_file(MODEL_FILE_NAME_KEY, MODEL_PATH)
+
+print('loading model...')
+#Load the model for Predictions
+model = load(MODEL_PATH)
+print('model loaded\n')
+
+
+def lambda_handler(event, context):
+    body = dict()
+    
+    if (event['httpMethod'] == 'GET'):
+        params = event['queryStringParameters']
+    else:
+        params = json.loads(event['body'])
+    
+    if params is not None and 'words' in params:
+        x = [params['words']]
+        print(x)
+        
+        # predict document classes and decode predictions
+        predictions = model.predict(x)
+        
+        body['prediction'] = predictions[0]
+        body['confidence'] = predictions[0]
+    
+    #Create the response
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body),
+        "headers": {
+            "Content-Type": "application/json",
+            "X-Custom-Header": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE",
+            "Access-Control-Allow-Headers": "X-Requested-With,content-type"
+        }
+
+    }
+
+    return response
--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -2,7 +2,7 @@ import json
 
 import pytest
 
-from hello_world import app
+from lambda_helper import app
 
 
 @pytest.fixture()
@@ -10,7 +10,7 @@ def apigw_event():
     """ Generates API GW Event"""
 
     return {
-        "body": '{ "test": "body"}',
+        "body": '{ "words": "this is a test"}',
         "resource": "/{proxy+}",
         "requestContext": {
             "resourceId": "123456",
@@ -34,7 +34,7 @@ def apigw_event():
             },
             "stage": "prod",
         },
-        "queryStringParameters": {"foo": "bar"},
+        "queryStringParameters": {"words": "bar"},
         "headers": {
             "Via": "1.1 08f323deadbeefa7af34d5feb414ce27.cloudfront.net (CloudFront)",
             "Accept-Language": "en-US,en;q=0.8",
@@ -68,6 +68,6 @@ def test_lambda_handler(apigw_event, mocker):
     data = json.loads(ret["body"])
 
     assert ret["statusCode"] == 200
-    assert "message" in ret["body"]
-    assert data["message"] == "hello world"
+    assert "prediction" in ret["body"]
+    assert "confidence" in ret["body"]
     # assert "location" in data.dict_keys()
